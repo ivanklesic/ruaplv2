314f2571d024d134b6b1d7e24f46f0e1b4fbbb4f
Ivan Klesic <ivan.klesic@gmail.com>
2018-04-09 21:03:02 +0200

    Commit3

--- WebApplication1/WebApplication1/Controllers/ContactController.cs
+++ WebApplication1/WebApplication1/Controllers/ContactController.cs
@@ -4,19 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
     public class ContactController : ApiController
     {
+        private ContactRepository contactRepository;
 
-        public string[] Get()
+        public ContactController()
         {
-            return new string[]
-            {
-        "Hello",
-        "World"
-            };
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
         }
     }
 }
